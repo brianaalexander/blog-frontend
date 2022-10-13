85c58b26d870f6f92dfe7da8a3cadaaf9090c0ed
brianaalexander <bribri0283@gmail.com>
2022-10-12 21:31:51 -0500

    added card

--- src/PostsIndex.jsx
+++ src/PostsIndex.jsx
@@ -3,15 +3,18 @@ export function PostsIndex(props) {
   return (
     <div id="posts-index">
       <h1>All Posts</h1>
-      <div className="posts row">
+
+      <div className="row">
         {props.posts.map((post) => (
-          <div className="col-4" key={post.id}>
-            <h2>{post.title}</h2>
-            <p>{post.body}</p>
-            <img src={post.image} alt="" />
-            <button class="btn btn-primary" onClick={() => props.onSelectPost(post)}>
-              More info
-            </button>
+          <div className="col-md-3 mb-4" key={post.id}>
+            <div className="card">
+              <img src={post.image} className="card-img-top" alt="..." />
+              <h2>{post.title}</h2>
+              <p>{post.body}</p>
+              <button class="btn btn-primary" onClick={() => props.onSelectPost(post)}>
+                More info
+              </button>
+            </div>
           </div>
         ))}
       </div>
