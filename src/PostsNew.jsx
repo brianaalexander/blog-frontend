bf9b2a53bd5ca4a495323e54b84a5aca414ff78e
brianaalexander <bribri0283@gmail.com>
2022-10-16 10:33:29 -0500

    making sure post new works, formatted card image

--- src/PostsNew.jsx
+++ src/PostsNew.jsx
@@ -1,16 +1,28 @@
+import axios from "axios";
+
 export function PostsNew() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("handleSubmit new post");
+    const params = new FormData(event.target);
+    axios.post("http://localhost:3000/posts.json", params).then((response) => {
+      console.log(response);
+      event.target.reset();
+    });
+  };
+
   return (
     <div id="posts-new">
       <h1>New Post</h1>
-      <form action="">
+      <form onSubmit={handleSubmit}>
         <div>
-          Title: <input className="form-control" type="text" />
+          Title: <input name="title" className="form-control" type="text" />
         </div>
         <div>
-          Body: <input className="form-control" type="text" />
+          Body: <input name="body" className="form-control" type="text" />
         </div>
         <div>
-          Image: <input className="form-control" type="text" />
+          Image: <input name="image" className="form-control" type="text" />
         </div>
         <button className="btn btn-primary mt-3" type="submit">
           Create
