31e3d7bd8b97572821bb69827795df8e2578b75a
brianaalexander <bribri0283@gmail.com>
2022-10-23 10:35:40 -0500

    added dynamic search filter for posts index

--- src/PostsIndex.jsx
+++ src/PostsIndex.jsx
@@ -1,22 +1,27 @@
+import { useState } from "react";
+
 export function PostsIndex(props) {
-  console.log(props);
+  const [searchFilter, setSearchFilter] = useState("");
   return (
     <div id="posts-index">
       <h1>All Posts</h1>
-
+      Search Filter:{" "}
+      <input type="text" value={searchFilter} onChange={(event) => setSearchFilter(event.target.value)} />
       <div className="row">
-        {props.posts.map((post) => (
-          <div className="col-md-3 mb-4" key={post.id}>
-            <div className="card">
-              <img src={post.image} className="card-img-top" alt="..." />
-              <h2>{post.title}</h2>
-              <p>{post.body}</p>
-              <button class="btn btn-primary" onClick={() => props.onSelectPost(post)}>
-                More info
-              </button>
+        {props.posts
+          .filter((post) => post.title.toLowerCase().includes(searchFilter.toLowerCase()))
+          .map((post) => (
+            <div className="col-md-3 mb-4" key={post.id}>
+              <div className="card">
+                <img src={post.image} className="card-img-top" alt="..." />
+                <h2>{post.title}</h2>
+                <p>{post.body}</p>
+                <button class="btn btn-primary" onClick={() => props.onSelectPost(post)}>
+                  More info
+                </button>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
       </div>
     </div>
   );
